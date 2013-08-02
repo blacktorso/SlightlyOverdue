d47ad25e87c729fcdd3b1d4772be3f7b5815b936
blacktorso <blueandyellowfl@gmail.com>
2013-08-02 13:42:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GettingEven::Application.config.secret_token = '07effb13dbe2d272f2f4c2d7b83662ea9c618ca7e68af617326ec27024d186ee99155fb96ebec514827f2f02e1576101f8f961038691032c129a240d803142e2'
